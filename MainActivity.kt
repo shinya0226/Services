7e643cc93b84b8e3a2d3af93c9a8e8375e6a85ef
shinya0226 <79456274+shinya0226@users.noreply.github.com>
2021-04-21 11:26:51 +0900

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.intentservice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnStartService.setOnClickListener {
+            Intent(this, MyService::class.java).also {
+                startService(it)
+                tvServiceInfo.text = "Service Running"
+            }
+        }
+
+        btnStopService.setOnClickListener {
+            Intent(this, MyService::class.java).also {
+                 stopService(it)
+            tvServiceInfo.text = "Service Stopped"
+        }
+
+        btnSendData.setOnClickListener {
+            Intent(this, MyService::class.java).also {
+                val dataString = etData.text.toString()
+                it.putExtra("EXTRA_DATA", dataString)
+                startService(it)
+            }
+        }
+        }
+
+
+    }
+}
